a31e4437e8941833126b8251f03cbb2d410193ab
Michal Nawrocki <v25@vault25.net>
2017-04-05 16:05:41 +0200

    Szkielet, zlikwidowac padding headera, bo menu bedzie przeszkadzac pozniej

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,54 @@
-var gulp = require('gulp'), 
-    sass = require('gulp-ruby-sass') 
-    notify = require("gulp-notify") 
-    bower = require('gulp-bower');
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass')
+notify = require("gulp-notify")
+bower = require('gulp-bower');
+var gsass = require('gulp-sass');
 
 var config = {
-     sassPath: './resources/sass',
-     bowerDir: './bower_components' 
+    sassPath: './resources/sass',
+    bowerDir: './bower_components'
 }
-
-gulp.task('bower', function() { 
+gulp.task('bower', function() {
     return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
+        .pipe(gulp.dest(config.bowerDir))
 });
-
-gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest('./public/fonts')); 
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./public/fonts'));
 });
-
-gulp.task('css', function() { 
+gulp.task('css', function() {
     return gulp.src(config.sassPath + '/style.scss')
-         .pipe(sass({
-             style: 'compressed',
-             loadPath: [
-                 './resources/sass',
-                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-             ]
-         }) 
-            .on("error", notify.onError(function (error) {
-                 return "Error: " + error.message;
-             }))) 
-         .pipe(gulp.dest('./public/css')); 
+        .pipe(sass({
+                style: 'compressed',
+                loadPath: [
+                    './resources/sass',
+                    config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+                    config.bowerDir + '/fontawesome/scss',
+                ]
+            })
+            .on("error", notify.onError(function(error) {
+                return "Error: " + error.message;
+            })))
+        .pipe(gulp.dest('./public/css'));
 });
 
+gulp.task('sass', function() {
+    return gulp.src(config.sassPath + '/style.scss')
+        .pipe(gsass({
+                outputStyle: 'compressed',
+                includePaths: [
+                    './resources/sass',
+                    config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+                    config.bowerDir + '/fontawesome/scss',
+                ]
+            })
+            .on("error", notify.onError(function(error) {
+                return "Error: " + error.message;
+            })))
+        .pipe(gulp.dest('./public/css'));
+});
 // Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['sass']);
 });
-
-  gulp.task('default', ['bower', 'icons', 'css']);
+gulp.task('default', ['bower', 'icons', 'css']);
